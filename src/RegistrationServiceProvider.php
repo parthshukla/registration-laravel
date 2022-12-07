54cab1981d80bbfe552144c115de691d6da62c5b
Parth Shukla <shuklaparth@hotmail.com>
2022-12-07 11:19:06 +0530

    Initial Commit

---
+++ src/RegistrationServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+namespace ParthShukla\Registration;
+
+use Illuminate\Support\ServiceProvider;
+
+/**
+ * RegistrationServiceProvider class
+ *
+ * @since 1.0.0
+ * @version 1.0.0
+ * @author Parth Shukla <parthshukla@ahex.co.in>
+ */
+class RegistrationServiceProvider extends ServiceProvider
+{
+
+    public function boot()
+    {
+
+    }
+
+    public function register()
+    {
+
+    }
+
+}
+// end of class RegistrationServiceProvider
+// end of file
